84570f7583bfd55aa26b6bde3c0ee94283668cb3
zh-kai <zh_kai0531@163.com>
2023-05-12 18:33:16 +0800

    chore: init test

---
+++ test/index.test.ts
@@ -0,0 +1,14 @@
+import { Contract, JsonRpcProvider } from 'ethers'
+
+it('测试 #1', async () => {
+  const address = '0xf44CB548eF9B21E9D5798f2d7d78367b727D0c28'
+  const FactoryAbi = ['function allPairs(uint) external view returns (address pair)']
+  // const provider = new JsonRpcProvider('https://testnet.era.zksync.dev', 280)
+
+  // const factoryContract = new Contract(address, FactoryAbi, provider)
+
+  // const res = await factoryContract.allPairsLength()
+  // console.log(res)
+})
+
+// it('测试 #1', async () => {})
